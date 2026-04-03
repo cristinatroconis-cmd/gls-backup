ba40c904488b9fc1d810e7952d1076b71e1c3a5b
Cristina Troconis <cristinatroconis@gmail.com>
2026-04-03 16:49:50 -0400

    Refactor archive-experiencias.php and home-publics-slides.php for improved structure and readability

--- archive-experiencias.php
+++ archive-experiencias.php
@@ -1,27 +1,19 @@
 <?php get_header();
 ?>
-<main id="main" class="site-main">
-<section class="cabecera cabecera--mg0">
-		<?php   $cabecera_experiencias = get_field('cabecera_experiencias','option');?>
-		<img src="<?php echo esc_url($cabecera_experiencias['url']); ?>" alt="<?php the_title(); ?>" class="" />
-		<div class="container centrar">        
-			<div class="row position-relative">
-				<div class="col-12 col-md-12 centrar">
-					<?php
-					$titulo_experiencias = get_field('titulo_experiencias','option');
-					$subtitulo_experiencias = get_field('subtitulo_experiencias','option');
-					?>
-					<h1 class="d-none">Experiencias turísticas en Granada</h1>			
-					<span class="h1 strong bellmt white"><?php echo $titulo_experiencias; ?></span>
-					<span class="h1 bellmt white"><?php echo $subtitulo_experiencias; ?></span>
-				</div>
-			</div>
-		</div>
+
+<?php
+/**
+ * GLS – Hero específico para páginas de archivo
+ */
+get_template_part('template-parts/gls-page-hero');
+
+?>
+<main>
 	</section>
 	<div id="experiencias-content">
 		<div class="TabHero-gradient"></div>
-		
-		<?php 
+
+		<?php
 		$categorias = get_terms([
 			'taxonomy' => 'category', // Cambia 'category' por tu taxonomía personalizada si es necesario
 			'hide_empty' => true, // Cambia a false si quieres incluir categorías sin posts
@@ -32,7 +24,7 @@
 			foreach ($categorias as $index => $categoria) {
 				// Verifica si esta categoría es la primera para asignarle la clase 'active' o no
 				$activeClass = $index === 0 ? 'active' : '';
-				?>
+		?>
 				<div class="tabs-child <?= $activeClass; ?>" data-target="<?= esc_attr($categoria->slug); ?>">
 					<?= _e($categoria->name, "experiencias"); ?>
 				</div>
@@ -46,16 +38,16 @@
 			echo '<div class="tabs-underline"></div>';
 			foreach ($categorias as $index => $categoria) {
 				$activeClass = $index === 0 ? 'active' : '';
-				echo '<div class="experiencias-description experiencias-description-'.esc_attr($categoria->slug).' '.$activeClass.'">'.esc_attr($categoria->description).'</div> ';
+				echo '<div class="experiencias-description experiencias-description-' . esc_attr($categoria->slug) . ' ' . $activeClass . '">' . esc_attr($categoria->description) . '</div> ';
 			}
 			echo '<section class="experiencias">
 				<img src="" alt="" id="experiencias-background" class="" />
 				';
-				echo '<div class="experiencias-container-slide">';
+			echo '<div class="experiencias-container-slide">';
 			foreach ($categorias as $index => $categoria) {
 				$activeClass = $index === 0 ? 'active' : '';
-				echo '<div id="'.esc_attr($categoria->slug).'" class="experiencias-category '.$activeClass.'">';
-				
+				echo '<div id="' . esc_attr($categoria->slug) . '" class="experiencias-category ' . $activeClass . '">';
+
 				$args = [
 					'post_type' => 'experiencias', // Tu custom post type
 					'posts_per_page' => -1, // Cantidad de posts por página, -1 para todos
@@ -70,47 +62,48 @@
 				$query = new WP_Query($args);
 
 				if ($query->have_posts()) {
-					
+
 					while ($query->have_posts()) {
 						$query->the_post();
 						$image_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
 						$image_id = 'img-' . $categoria->slug . '-' . get_the_ID();
-						?>
+				?>
 						<div class="experiencia-card">
-						<h4><?= get_the_title() ?></h4>
-						<div class="experiencia-img">
-							<img data-lazy="<?php echo esc_url($image_url); ?>" alt="<?php the_title(); ?>" id="<?=$image_id?>" />
+							<h4><?= get_the_title() ?></h4>
+							<div class="experiencia-img">
+								<img data-lazy="<?php echo esc_url($image_url); ?>" alt="<?php the_title(); ?>" id="<?= $image_id ?>" />
+							</div>
+							<div class="experiencia-contenido">
+								<div class="experiencia-excerpt">
+									<p><?= get_the_excerpt() ?></p><a class="experiencia-link" href="<?= get_permalink() ?>"><?= _e("VER EXPERIENCIA", "experiencias"); ?></a>
+								</div>
+
+							</div>
 						</div>
-						<div class="experiencia-contenido">
-							<div class="experiencia-excerpt"><p><?= get_the_excerpt()?></p><a class="experiencia-link" href="<?= get_permalink()?>"><?= _e("VER EXPERIENCIA", "experiencias"); ?></a></div>
-							
-						</div>
-						</div>
-						<?php
+		<?php
 					}
-					
 				}
 				echo '</div>';
-				
-			// Restablece el postdata global para evitar conflictos
+
+				// Restablece el postdata global para evitar conflictos
 				wp_reset_postdata();
 			}
-			
+
 			echo '</section>';
 			echo '</div>';
 		}
 
 		?>
-		
-			<!-- <div class="tabs-child" data-target="experiencia1"> <?= _e("Turismo", "experiencias"); ?>
+
+		<!-- <div class="tabs-child" data-target="experiencia1"> <?= _e("Turismo", "experiencias"); ?>
 			</div>
 			<div class="tabs-bullets"></div>
 			<div class="tabs-child active" data-target="experiencia2"><?= _e("Gastronomía", "experiencias"); ?></div>
 			<div class="tabs-bullets"></div>
 			<div class="tabs-child" data-target="experiencia3"><?= _e("Naturaleza", "experiencias"); ?></div> -->
-		    	
+
 	</div>
-	
-	
+
+
 </main>
 <?php get_footer(); ?>
